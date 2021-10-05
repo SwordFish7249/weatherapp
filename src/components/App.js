f1a88f48ee3ba0ff686e8bb9f082706c532677cf
Erhan11 <erhan0@hotmail.com>
2021-10-05 13:45:38 +0300

    Just before testing components

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,26 @@
 import React from "react";
+import PropTypes from "prop-types";
+import "../styles/App.css";
+import LocationDetails from "./LocationDetails";
 
-const App = () => <h1>Hello World</h1>;
+const App = ({ location }) => {
+  const { city, country } = location;
+  return <LocationDetails city={city} country={country} />;
+};
+App.propTypes = {
+  location: PropTypes.shape({
+    city: PropTypes.string,
+    country: PropTypes.string,
+  }).isRequired,
+};
+
+// const App = () => {
+// return (
+// <div className="App">
+// <LocationDetails />
+// </div>
+// );
+// };
 
 // The above utilises an implicit return, which is a feature of arrow-functions.
 // It is the same as this:
